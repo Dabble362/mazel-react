82df3d8827f0af1153ff9787d7a08d030f347fcc
Jed Duffey <jedduffey@gmail.com>
2023-07-20 15:59:29 -0400

    Install react-router-dom and use built-in router components to handle page changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,41 @@
-import { useState, useEffect } from 'react';
+// import { useState, useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
 
 import WelcomeBox from './components/WelcomeBox';
 import LoginBox from './components/LoginBox';
 
 function App() {
+  // Keeping this here for now as an example of how to talk to the server
 
-  const [serverResponse, setServerResponse] = useState(null);
+  // const [serverResponse, setServerResponse] = useState(null);
 
-  useEffect(() => {
-    fetch('http://localhost:2500/test')
-      .then(res => res.json())
-      .then(data => {
-        console.log(data.message);
-        setServerResponse(`${data.message} at ${data.currentTime}`);
-      })
+  // useEffect(() => {
+  //   fetch('http://localhost:2500/test')
+  //     .then(res => res.json())
+  //     .then(data => {
+  //       console.log(data.message);
+  //       setServerResponse(`${data.message} at ${data.currentTime}`);
+  //     })
 
-  }, [])
+  // }, [])
 
   return (
     <div className="App">
-      <p>{serverResponse}</p>
-      <LoginBox />
-      <WelcomeBox />
-    </div>
+      <Routes>
+        <Route
+          path="/"
+          element={<WelcomeBox />}
+        />
+        <Route
+          path="login"
+          element={<LoginBox />}
+        />
+        <Route
+          path="*"
+          element={<h1>404</h1>} // TODO: replace with actual 404 page
+        />
+      </Routes>
+    </div >
   );
 }
 
