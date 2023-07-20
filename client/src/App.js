79d222a7668294766d3df82f2ff8b0975e12c237
Dabble362 <75595585+Dabble362@users.noreply.github.com>
2023-07-20 12:21:02 -0400

    install cors, backend talk to front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  const [serverResponse, setServerResponse] = useState(null);
+
+  useEffect(() => {
+    fetch('http://localhost:2500/test')
+      .then(res => res.json())
+      .then(data => {
+        console.log(data.message);
+        setServerResponse(`${data.message} at ${data.currentTime}`);
+      })
+
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>{serverResponse}</p>
     </div>
   );
 }
--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
+
+app.use(cors());
 
 const port = 2500;
 app.listen(port, () => {
   console.log(`App is listening on port: ${port}`)
 });
 
-app.get('/', (req, res) => {
-  res.send('You all up innit now')
+app.get('/test', (req, res) => {
+  res.send({
+    message: "type a message",
+    currentTime: new Date(),
+  })
 });
